5c10a853ffe2f85c4f5579352307bbc58bfe7a83
vladdavis2416 <sockolow.vladisl@gmail.com>
2024-07-29 14:58:35 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -2,8 +2,9 @@ my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 lenght = len(my_list)
 c = 0
 while c<lenght and my_list[c]>-1:
-    print(my_list[c])
-    c=c+1
-
-
+    if my_list[c] == 0:
+        c=c+1
+    else:
+        print(my_list[c])
+        c=c+1
 
